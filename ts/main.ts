4447306abbc10f4df1b774c363074296a8fcb74b
Nick George <me@izzmo.com>
2016-03-31 14:21:31 -0500

    Added GTM API, TSWS, TeamStore, and Coach's Assistant service checkers.

--- ts/main.ts
+++ ts/main.ts
@@ -3,12 +3,20 @@ import CampaignMonitorService = require('./services/CampaignMonitorService');
 import AuthorizeNetService = require('./services/AuthorizeNetService');
 import GtmSportswearService = require('./services/GtmSportswearService');
 import GtmSportswearHttpsService = require('./services/GtmSportswearHttpsService');
+import GtmApiService = require('./services/GtmApiService');
+import CoachsAssistantService = require('./services/CoachsAssistantService');
+import TeamStoreService = require('./services/TeamStoreService');
+import TSWService = require('./services/TSWService');
 
 let services = [
   new CampaignMonitorService.CampaignMonitorService(),
   new AuthorizeNetService.AuthorizeNetService(),
   new GtmSportswearService.GtmSportswearService(),
-  new GtmSportswearHttpsService.GtmSportswearHttpsService()
+  new GtmSportswearHttpsService.GtmSportswearHttpsService(),
+  new GtmApiService.GtmApiService(),
+  new CoachsAssistantService.CoachsAssistantService(),
+  new TeamStoreService.TeamStoreService(),
+  new TSWService.TeamStoreWebService()
 ];
 
 let sm = new ServiceManager.ServiceManager(services);
