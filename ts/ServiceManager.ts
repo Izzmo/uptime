f654156a8f5ed2a5b19f4055e012957e0ab2860c
Nick George <me@izzmo.com>
2016-05-10 20:10:29 -0500

    Update service manager to adhere to error interval when a service becomes in error state.

--- ts/ServiceManager.ts
+++ ts/ServiceManager.ts
@@ -1,24 +1,25 @@
 import { IService } from './IService';
 import { IStatus } from './IStatus';
 
-interface IServiceInterval {
+interface ServiceInterval {
   service: IService;
   timer: NodeJS.Timer;
-  history: Array<IStatus>;
+  history: IStatus[];
+  inErrorMode: boolean;
 }
 
 export class ServiceManager {
-  private serviceList: IServiceInterval[] = [];
+  private serviceList: ServiceInterval[] = [];
   private updateCallback: Function;
 
   constructor(serviceList: Array<IService>) {
     serviceList.forEach((service) => {
-      this.serviceList.push({ service: service, timer: null, history: [] });
+      this.serviceList.push({ service: service, timer: null, history: [], inErrorMode: false });
     });
   }
 
   public setUpdateCallback(fn: Function): void {
-    if(undefined !== fn && null !== fn)
+    if (undefined !== fn && null !== fn)
       this.updateCallback = fn;
   }
 
@@ -26,16 +27,35 @@ export class ServiceManager {
     this.serviceList.forEach(service => {
       if (service.timer !== null) clearInterval(service.timer);
 
-      let callService = () => {
+      console.log(`Service start: ${service.service.description}`);
+
+      const callService = () => {
         service.service.getStatus().then(s => {
+          if (s.hasError) {
+            if (!service.inErrorMode) {
+              console.log('toggle');
+              service.inErrorMode = true;
+
+              clearInterval(service.timer);
+              service.timer = setInterval(() => {
+                callService();
+              }, 1000 * service.service.errorCheckInterval);
+            }
+          }
+          else if (service.inErrorMode) {
+            service.inErrorMode = false;
+
+            clearInterval(service.timer);
+            service.timer = setInterval(() => {
+              callService();
+            }, 1000 * service.service.checkIntervalInSeconds);
+          }
+
           this.updateCallback(service.service, s);
         });
       };
 
       callService();
-      service.timer = setInterval(() => {
-        callService();
-      }, 1000 * service.service.checkIntervalInSeconds);
     });
   }
 
